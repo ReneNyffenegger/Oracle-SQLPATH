b757a51c088983f33f0d827676b68ee160978c90
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-10-15 20:23:23 +0200

    column widths

--- err.sql
+++ err.sql
@@ -1,1 +1,11 @@
-select name, type, line, position, substr(text, 1, 90) from all_errors;
+column name format a30
+column text format a90
+
+select
+   substr(name, 1, 30) name,
+   type,
+   line,
+   position,
+   substr(text, 1, 90) text
+from
+   all_errors;
