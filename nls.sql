6bce324c169d9430cb9321402e7d89d99108e826
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-12-28 13:20:06 +0100

    db, inst, ses

--- nls.sql
+++ nls.sql
@@ -2,7 +2,14 @@
 --   Compare with -> param_nls.sql
 --
 select
-  parameter,
-  substrb(value,1,40)
+   substrb(d.parameter, 1, 25) parameter,
+   substrb(S.value    , 1, 35) ses,
+   substrb(d.value    , 1, 35) db,
+   substrb(I.value    , 1, 35) inst
 from
-  nls_database_parameters;
+   nls_database_parameters d                              left join
+   nls_session_parameters  s on d.parameter = s.parameter left join
+   nls_instance_parameters i on d.parameter = i.parameter
+order by
+   parameter
+;
