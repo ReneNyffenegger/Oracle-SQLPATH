8a2ce7dac0abb6cbc1857078bc6d56d2c8c9ee23
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-07-08 21:47:37 +0200

    add more columns

--- who-am-i.sql
+++ who-am-i.sql
@@ -1,12 +1,12 @@
---
--- Who am I?
---
--- See also
---   -> mypid.sql
---   -> mysid.sql
---
 select
-  substr(sys_context('USERENV', 'CURRENT_SCHEMA'), 1, 30) current_schema,
-  substr(sys_context('USERENV', 'SESSION_USER'  ), 1, 30) session_user
+   substr   (user                                    , 1, 30) usr,
+   substr   (sys_context('userenv', 'current_schema'), 1, 30) current_schema,
+   to_number(sys_context('userenv', 'sid'           )    ) sid,
+   substr   (sys_context('userenv', 'session_user'  ), 1, 30) session_user,
+   substr   (sys_context('userenv', 'db_name'       ), 1, 30) db_name,
+   substr   (            ora_database_name           , 1, 30) ora_database_name,
+   substrb  (            global_name                 , 1, 30) global_name,
+   substr   (sys_context('userenv', 'server_host'   ), 1, 30) server_host,
+   substr   (sys_context('userenv', 'service_name'  ), 1, 30) service_name
 from
-  dual;
+   global_name;
